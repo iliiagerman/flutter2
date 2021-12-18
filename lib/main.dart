171f225bf41d432e67ec9c4f0d0e32db94e14db2
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-12-18 15:41:09 +0200

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  return runApp(MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: const Text('My First App'),
+          title: Text('My First App'),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text(
-            'This is My Homepage',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                color: Colors.white),
+        body: Center(
+          child: Container(
+            child: Column(
+
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
