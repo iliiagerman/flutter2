fe6a20003b1ba36efa36c67306b9e98ed24081d1
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-12-18 15:57:50 +0200

    add floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,26 @@ class MyFirstApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
+            padding: EdgeInsets.all(16),
             child: Column(
-
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                LinearProgressIndicator(value: 23),
+                Text(
+                  '23 %',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  'Press button to download',
+                  style: TextStyle(color: Colors.white, fontSize: 20)
+                ),
+              ],
             ),
           ),
+        ),
+        floatingActionButton: const FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.cloud_download),
         ),
       ),
     );
