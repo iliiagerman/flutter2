3ab57898fe65746e096e9d28d72440fe4cc26127
iliia Germaniuk <iliia.germaniuk@gmail.com>
2022-01-17 14:11:25 +0200

    no message

---
+++ lib/screens/screen2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Screen2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: ,
+        ),
+      ),
+    );
+  }
+}
