c3f9e3e2555765e26faa69e2869d51460a4efdc3
iliia Germaniuk <iliia.germaniuk@gmail.com>
2022-01-20 18:30:53 +0200

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_second/screens/settings/settings_list_item.dart';
 import 'package:flutter_second/task1/user_list_item_widget.dart';
 import 'package:flutter_second/user_util.dart';
-import 'package:flutter_svg/svg.dart';
 
 void main() {
-  return runApp(SettingsPage());
+  return runApp(const SettingsPage());
 }
 
 class SettingsPage extends StatelessWidget {
@@ -18,19 +16,27 @@ class SettingsPage extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.grey,
         body: Container(
-          padding: EdgeInsets.only(left: 15, right: 15),
+          padding: const EdgeInsets.only(left: 15, right: 15),
           child: SafeArea(
-            child: Center(
-              child: Container(
-                height: 66,
-                color: Colors.white,
-                child: UserListItemWidget(
-                  selected: false,
-                  user: UserUtil.usersList.first,
-                  onClick: () {},
-                ),
+            child: Column(children: [
+              UserListItemWidget(
+                selected: false,
+                user: UserUtil.usersList.elementAt(0),
+                onClick: () {},
               ),
-            ),
+              const SizedBox(height: 6,),
+              UserListItemWidget(
+                selected: false,
+                user: UserUtil.usersList.elementAt(1),
+                onClick: () {},
+              ),
+              const SizedBox(height: 6,),
+              UserListItemWidget(
+                selected: false,
+                user: UserUtil.usersList.elementAt(2),
+                onClick: () {},
+              ),
+            ]),
           ),
         ),
       ),
--- lib/task1/user_list_item_widget.dart
+++ lib/task1/user_list_item_widget.dart
@@ -6,23 +6,37 @@ class UserListItemWidget extends StatelessWidget {
   final bool selected;
   final VoidCallback onClick;
 
-  const UserListItemWidget(
-      {Key? key,
-      required this.user,
-      required this.selected,
-      required this.onClick})
+  const UserListItemWidget({Key? key,
+    required this.user,
+    required this.selected,
+    required this.onClick})
       : super(key: key);
 
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 66,
+      color: Colors.white,
       padding: const EdgeInsets.all(16),
-      child: Row(children: [
-        Text(user.name, ),
-        Text(user.secondName, ),
-      ]),
+      child: Center(
+        child: Row(children: [
+           CircleAvatar(
+           backgroundColor: Colors.grey,
+            child: Text(
+              user.name[0],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+            Text(user.name + ' ' + user.secondName),
+            Visibility(
+                    visible: user.role != null,
+                    child: Text(user.role ?? '')),
+          ]),
+        ]),
+      ),
     );
-
   }
 }
