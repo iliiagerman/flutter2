5bff35fe8c4c4fa97ea5ee92acf9bd7f6ec599de
iliia Germaniuk <iliia.germaniuk@gmail.com>
2022-01-26 19:50:43 +0200

    no message

--- lib/task1/user_list_item_widget.dart
+++ lib/task1/user_list_item_widget.dart
@@ -15,51 +15,54 @@ class UserListItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 66,
-      padding: const EdgeInsets.all(16),
-      child: Center(
-        child: Row(children: [
-          CircleAvatar(
-            backgroundColor: Colors.grey,
-            backgroundImage: user.image,
-            child: user.image == null
-                ? Text(
-                    user.name[0],
-                  )
-                : const SizedBox(
-                    height: 0,
-                    width: 0,
-                  ),
-          ),
-          Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(user.name + ' ' + user.secondName),
-                Visibility(
-                    visible: user.role != null, child: Text(user.role ?? '')),
-              ]),
-          Spacer(),
-          // SizedBox(
-          //   width: 28,
-          //   height:28,
-          //   child: Container(
-          //     decoration: BoxDecoration(
-          //         color: const Color(0x2CA09F9F),
-          //         borderRadius: BorderRadius.circular(12)),
-          //   ),
-          // ),
-          const Icon(
-            Icons.check_circle,
-            color: Colors.blue,
-            size: 30,
-          ),
-        ]),
-      ),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10)
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 66,
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Row(children: [
+            CircleAvatar(
+              backgroundColor: Colors.grey,
+              backgroundImage: user.image,
+              child: user.image == null
+                  ? Text(
+                      user.name[0],
+                    )
+                  : const SizedBox(
+                      height: 0,
+                      width: 0,
+                    ),
+            ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(user.name + ' ' + user.secondName),
+                  Visibility(
+                      visible: user.role != null, child: Text(user.role ?? '')),
+                ]),
+            Spacer(),
+            // SizedBox(
+            //   width: 28,
+            //   height:28,
+            //   child: Container(
+            //     decoration: BoxDecoration(
+            //         color: const Color(0x2CA09F9F),
+            //         borderRadius: BorderRadius.circular(12)),
+            //   ),
+            // ),
+            const Icon(
+              Icons.check_circle,
+              color: Colors.blue,
+              size: 30,
+            ),
+          ]),
+        ),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10)
+        ),
       ),
     );
   }
