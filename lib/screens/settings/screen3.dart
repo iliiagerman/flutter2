07977b6c7d20e81e7484b9806f8e3e486dc1142b
iliia Germaniuk <iliia.germaniuk@gmail.com>
2022-01-28 14:58:25 +0200

    no message

---
+++ lib/screens/settings/screen3.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_second/task1/user_list_item_widget.dart';
+import 'package:flutter_second/user_util.dart';
+
+void main() {
+  return runApp(const SettingsPage());
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Container(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: SafeArea(
+            child: Column(children: [
+              UserListItemWidget(
+                selected: false,
+                user: UserUtil.usersList.elementAt(0),
+                onClick: () {},
+              ),
+              const SizedBox(height: 6,),
+              UserListItemWidget(
+                selected: false,
+                user: UserUtil.usersList.elementAt(1),
+                onClick: () {},
+              ),
+              const SizedBox(height: 6,),
+              UserListItemWidget(
+                selected: false,
+                user: UserUtil.usersList.elementAt(2),
+                onClick: () {},
+              ),
+              const SizedBox(height: 6,),
+              UserListItemWidget(
+                selected: false,
+                user: UserUtil.usersList.elementAt(3),
+                onClick: () {},
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
