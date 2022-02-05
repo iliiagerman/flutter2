3ec1ec87ef072e23dfa4ba1c22eff611e378fe62
iliia Germaniuk <iliia.germaniuk@gmail.com>
2022-02-05 13:05:53 +0200

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_second/screens/form_text_field.dart';
 import 'package:flutter_second/screens/screen2.dart';
-import 'package:flutter_second/screens/screen4.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return const MaterialApp(
-      home: TextFormField2(),
+      home: SettingsPageScreen2(),
     );
   }
 }
