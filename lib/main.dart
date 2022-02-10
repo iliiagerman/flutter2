bdc18e72d1d3ad81cdb69b3b5acaf0fc145e6d73
iliia Germaniuk <iliia.germaniuk@gmail.com>
2022-02-10 14:28:09 +0200

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return const MaterialApp(
-      home: WeatherForecast(),
+      home: TextFormField2(),
     );
   }
 }
